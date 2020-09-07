a0a1f2ba41239f6fbdfa312f95b2261487dd4c13
Byron Verzmoter <byronverzmoter@gmail.com>
2020-09-07 13:08:22 +0000

    Basic distance measure done

--- tof_i2c_test.py
+++ tof_i2c_test.py
@@ -1,3 +1,26 @@
 import Adafruit_GPIO.I2C as I2C
+import qwiic_vl53l1x
+import time
 
-i2c = I2C(0x29) 
+# device = I2C.Device(0x29,2)
+# def_dev = I2C.get_i2c_device(I2C.get_default_bus())
+# print(dir(def_dev._bus))
+# # while True:
+#     # byte = i2c.readU8
+
+mySensor = qwiic_vl53l1x.QwiicVL53L1X(address = 0x29)
+print(mySensor.sensor_init())
+if (mySensor.sensor_init() == None):
+    print("Sensor online")
+
+start = time.time()
+timer = 0.0    
+while timer<3.0:
+    mySensor.start_ranging()
+    time.sleep(.005)
+    distance = mySensor.get_distance()
+    time.sleep(.005)
+    mySensor.stop_ranging()
+    timer = time.time() - start
+    
+    print("Distance {}".format(distance))
