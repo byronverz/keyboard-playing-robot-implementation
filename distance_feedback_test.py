00863b6120cd9688ed35687d22447c15e7325c78
Byron Verzmoter <byronverzmoter@gmail.com>
2020-09-08 21:01:31 +0000

    End of day commit

--- distance_feedback_test.py
+++ distance_feedback_test.py
@@ -2,6 +2,7 @@ import qwiic_vl53l1x
 import time
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
+import sys
 
 FREQUENCY = 1000
 
@@ -13,24 +14,28 @@ SERVO_PWM = 'P9_16'
 
 GPIO.setup(DIR_PIN, GPIO.OUT)
 GPIO.setup(EN_PIN, GPIO.OUT)
-GPIO.output(EN_PIN,GPIO.LOW)
-GPIO.output(DIR_PIN,GPIO.HIGH)
+# GPIO.output(EN_PIN,GPIO.LOW)
+# GPIO.output(DIR_PIN,GPIO.HIGH)
 
 # ToF sensor setup
 mySensor = qwiic_vl53l1x.QwiicVL53L1X(address = 0x29)
+init = mySensor.sensor_init()
+# if init != 0:
+    # print("Failed to initialize sensor")
+    # sys.exit()
 mySensor.set_distance_mode(1)
 print(mySensor.sensor_init())
 if (mySensor.sensor_init() == None):
     print("Sensor online")
 
-PWM.start(MOTOR_PWM,50,FREQUENCY)
-PWM.start(SERVO_PWM,50,120)
+# PWM.start(MOTOR_PWM,50,FREQUENCY)
+# PWM.start(SERVO_PWM,50,120)
 while True:
     mySensor.start_ranging()
     time.sleep(.005)
     distance = mySensor.get_distance()
     time.sleep(.005)
     mySensor.stop_ranging()
-    PWM.set_frequency(MOTOR_PWM,(distance*10))
-    PWM.set_duty_cycle(SERVO_PWM, (distance/10))
+    # PWM.set_frequency(MOTOR_PWM,(distance*10))
+    # PWM.set_duty_cycle(SERVO_PWM, (distance/10))
     print("Distance: {} mm".format(distance))
--- stepper_test.py
+++ stepper_test.py
@@ -10,28 +10,31 @@ GPIO.setup(DIR_PIN, GPIO.OUT)
 GPIO.setup(EN_PIN, GPIO.OUT)
 GPIO.output(EN_PIN,GPIO.LOW)
 GPIO.output(DIR_PIN,GPIO.HIGH)
-
-def step_speed_test (frequency,start_time):
+PWM.start(pwm_pin,50,1000)
+def step_speed_test (frequency, duty):
     print("Stepping at frequency {} Hz".format(frequency))
-    PWM.start(pwm_pin,50,frequency)
+    PWM.set_frequency(pwm_pin,frequency)
+    PWM.set_duty_cycle(pwm_pin, duty)
     # wait = 1/(2*frequency)
     end_time = 0.0
-    while (end_time-start_time) < 5.0:
-        end_time = time.time()    
+    # while (end_time-start_time) < 5.0:
+        # end_time = time.time()    
         # GPIO.output(pin, GPIO.HIGH)
         # time.sleep(wait)
         # GPIO.output(pin, GPIO.LOW)
         # time.sleep(wait)
     new_frequency = input("New frequency? :")
-    if new_frequency == 'x':
+    # new_frequency = 1000
+    new_duty = input("New duty? :")
+    if (new_frequency ) == 'x':
         PWM.stop(pwm_pin)
         PWM.cleanup()
         return
-    step_speed_test(int(new_frequency),time.time())
+    step_speed_test(int(new_frequency), int(new_duty))
     
     
 def main():
-    step_speed_test(1000,time.time())
+    step_speed_test(1000, 50)
     
 if __name__ == "__main__":
     main()
